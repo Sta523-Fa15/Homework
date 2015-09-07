095921dcd01bdfddf66de17c38762fbf01155ffc
Colin Rundel <rundel@gmail.com>
2015-09-07 09:54:35 -0400

    Check allowed files

---
+++ hw0/hw0_whitelist.R
@@ -0,0 +1,15 @@
+allowed_files = c("hw0.Rmd",
+                  "README.md,
+                  wercker.yml")
+
+pass = TRUE
+for(file in dir())
+{
+  if (!(file %in% allowed_files))
+  {
+    cat("Disallowed file:",file,"\n")
+    pass = FALSE
+  }
+}
+
+stopifnot(pass)
