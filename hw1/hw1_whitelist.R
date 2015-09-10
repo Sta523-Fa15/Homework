fa14fb295454b043b2cac82d7bab39083482b976
Colin Rundel <rundel@gmail.com>
2015-09-10 15:44:34 -0400

    initial hw1 tests

---
+++ hw1/hw1_whitelist.R
@@ -0,0 +1,19 @@
+allowed_files = c("hw1.Rmd",
+                  "README.md",
+                  "wercker.yml",
+                  "graph.R",
+                  "extra_credit.R")
+
+files = dir()
+disallowed_files = files[!(files %in% allowed_files)]
+
+if (length(disallowed_files != 0))
+{
+  cat("Disallowed files found:\n")
+  cat("  (remove the following files from your repo)\n\n")
+
+  for(file in disallowed_files)
+    cat("*",file,"\n")
+
+  quit("no",1,FALSE)
+}
