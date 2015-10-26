90261ad69bef737654077bc6794064c7a6b99d08
Colin Rundel <rundel@gmail.com>
2015-10-26 13:21:13 -0400

    Fix scoring

--- hw3/update_score.R
+++ hw3/update_score.R
@@ -3,7 +3,7 @@ library(rgdal)
 
 args = commandArgs(trailingOnly = TRUE)
 
-stopifnot(length(args) != 1)
+stopifnot(length(args) == 1)
 team = args[1]
 
 if (!file.exists("boroughs.json"))
@@ -11,11 +11,11 @@ if (!file.exists("boroughs.json"))
     stop("No boroughs.json to score!")    
 } 
 
-pred = readOGR("boroughs.json", layer="OGRGeoJSON")
+pred = readOGR("boroughs.json", layer="OGRGeoJSON", verbose=FALSE)
 load(file = "nybb.Rdata")
 
 
-stopifnot("Name" %in% pred@data)
+stopifnot("Name" %in% names(pred@data))
 
 score = 0
 for(b in bb@data$BoroName)
@@ -26,9 +26,9 @@ for(b in bb@data$BoroName)
     if (length(pred_i) == 0)
         stop("Borough of ",b," missing from predicted boundaries.")
 
-    score = score + gArea(gSymdifference(pred[pred_i,], bb[bb_i,]))
+    score = score + suppressWarnings(gArea(gSymdifference(pred[pred_i,], bb[bb_i,])))
 }
 
 score = score / gArea(bb)
 
-writeLines(toJSON(list("score" = score)), paste0(team,".json"))
+writeLines(toJSON(list("score" = score), auto_unbox=TRUE), paste0(team,".json"))
