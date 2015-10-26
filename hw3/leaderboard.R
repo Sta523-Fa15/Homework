d0ebbefa7c1137abed9d761145a13ceb0f4f8852
Colin Rundel <rundel@gmail.com>
2015-10-26 13:39:24 -0400

    Show leaderboard

---
+++ hw3/leaderboard.R
@@ -0,0 +1,14 @@
+suppressMessages(library(knitr))
+suppressMessages(library(jsonlite))
+suppressMessages(library(stringr))
+
+files = dir(pattern = "Team.*\\.json")
+
+d = data.frame(
+    teams = str_replace(files,"\\.json",""),
+    scores = unlist(lapply(files, fromJSON))
+)
+
+s = order(d$scores,decreasing=FALSE)
+
+kable(d[s,],row.names = FALSE)
